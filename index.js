f06e3e0d851da31209ca0cc9a595072ff137c633
royeradames <royeraadames@gmail.com>
2020-09-02 12:22:28 -0400

    init

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const server = require('./server.js');
+const helmet = require('helmet')
+server.use(helmet)
 
 server.listen(4000, () => {
   console.log('\n* Server Running on http://localhost:4000 *\n');
